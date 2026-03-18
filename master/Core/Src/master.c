b9898b3c234cdf96b5cc0156d01cfcce30e1ebae
Davide Alibrandi <davidealibrandi2000@gmail.com>
2026-03-18 22:04:54 +0100

    convert init struct to 8-bit command for uart transmission

--- master/Core/Src/master.c
+++ master/Core/Src/master.c
@@ -1,15 +1,18 @@
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "master.h"
 #include "gpio.h"
+#include "stm32g4xx.h"
+#include "stm32g4xx_hal_uart.h"
 #include "usart.h"
 
 typedef enum {KELVIN = 0, CELSIUS = 1} type_temp;
 typedef enum {NO_LOG = 0, LOG     = 1} type_log;
-typedef enum {ZERO_S = 0, TEN_S   = 1, THIRTY_S = 2, NINETY = 3} type_samp;
+typedef enum {ZERO_S = 0, TEN_S   = 1, THIRTY_S = 2, NINETY_S = 3} type_samp;
 typedef enum {MODE_SLAVE = 0, MODE_MASTER = 1} type_mode;
 
-struct {
+struct config {
     type_temp temperature;
     type_log log_time;
     type_samp samp_time;
@@ -23,7 +26,9 @@ struct {
     .save_setting = true
 };
 
-static inline void setting_slave(void); /* set slave before start modality master */
+static uint8_t command = 0;
+
+static inline uint8_t setting_slave(void); /* set slave before start modality master */
 static inline void run_mode_slave(void);
 static inline void run_mode_master(void);
 
@@ -34,7 +39,10 @@ MX_USART2_UART_Init();
 }
 
 void MASTER_run() {
-    if(init.save_setting) setting_slave();
+    if(init.save_setting) {
+        command = setting_slave();
+        HAL_UART_Transmit(&huart1, &command, sizeof(command), 100);
+    }
     switch (init.mode) {
         case MODE_SLAVE:
             run_mode_slave();
@@ -48,8 +56,69 @@ void MASTER_run() {
     }
 }
 
-static inline void setting_slave(void) {
+static inline uint8_t setting_slave(void) {
+    uint8_t command = 0;
+    init.save_setting = false;
+    switch (init.temperature) {
+        case KELVIN:
+            SET_BIT(command, 0 << 0);
+            break;
+        case CELSIUS:
+            SET_BIT(command, 1 << 0);
+            break;
+        default:
+            /* set kelvin */
+            SET_BIT(command, 1 << 0);
+            break;
+    }
+    
+    switch (init.log_time) {
+        case LOG:
+            SET_BIT(command, 1 << 1);
+            break;
+        case NO_LOG:
+            SET_BIT(command, 0 << 1);
+            break;
+        default:
+            /* set no log */
+            SET_BIT(command, 0 << 1);
+            break;
+    }
 
+    switch (init.samp_time) {
+        case ZERO_S:
+            SET_BIT(command, 0 << 3);
+            break;
+        case TEN_S:
+            SET_BIT(command, 1 << 3);
+            break;
+        case THIRTY_S:
+            SET_BIT(command, 2 << 3);
+            break;
+        case NINETY_S:
+            SET_BIT(command, 7 << 3);
+            break;
+        default:
+            /* set zero */
+            SET_BIT(command, 0 << 3);
+            break;
+    }
+
+    /* set no call*/
+    SET_BIT(command, 0 << 7);
+    /* set save setting on slave */
+    SET_BIT(command, 1 << 6);
+    /* set slave mode */
+    switch (init.samp_time) {
+        case ZERO_S:
+            SET_BIT(command, 0 << 2);
+            break;
+        default:
+            SET_BIT(command, 1 << 2);
+            break;
+    }
+
+    return command;
 }
 
 static inline void run_mode_slave(void) {
@@ -57,5 +126,5 @@ static inline void run_mode_slave(void) {
 }
 
 static inline void run_mode_master(void) {
-    
+
 }
